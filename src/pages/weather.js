1dc5f8c99f2e4d1e823f435f580b81f03912ceb7
Mohammad Ghasemi <mohstarsos1@Mohammads-MacBook-Pro.local>
2022-07-26 19:13:54 +0430

    add weather api

--- src/pages/weather.js
+++ src/pages/weather.js
@@ -1,5 +1,22 @@
+import axios from "axios";
+
 function Weather() {
-  return <div>weather</div>;
+  async function getWeather() {
+    try {
+      const res = await axios.get(
+        "http://api.weatherapi.com/v1/current.json?key=8254965796544b2cad840308211703&q=Iran/Kerman&aqi=no"
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={() => getWeather()}>get</button>
+    </div>
+  );
 }
 
 export default Weather;
