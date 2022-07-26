e797cbeb6f733c62c95195ecc621939934405269
Mohammad Ghasemi <mohstarsos1@Mohammads-MacBook-Pro.local>
2022-07-26 18:29:04 +0430

    simple is href

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 function NavItem({ children, link, isHref }) {
   return (
     <>
-      {!isHref && (
+      {!isHref ? (
         <Link Link to={link}>
           <li className="p-1 px-2 bg-teal-800 rounded shadow-lg text-white hover:bg-teal-600 hover:cursor-pointer active:bg-teal-500 transition-all">
             {children}
           </li>
         </Link>
-      )}
-      {isHref && (
+      ) : (
         <a
           href={link}
           className="p-1 px-2 bg-teal-800 rounded shadow-lg text-white hover:bg-teal-600 hover:cursor-pointer active:bg-teal-500 transition-all"
