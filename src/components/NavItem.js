53a13fe9b1064793954d980348fe775a02e17193
Mohammad Ghasemi <mohstarsos1@Mohammads-MacBook-Pro.local>
2022-08-10 18:48:42 +0430

    remove extra link word

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -4,7 +4,7 @@ function NavItem({ children, link, isHref }) {
   return (
     <>
       {!isHref ? (
-        <Link Link to={link}>
+        <Link to={link}>
           <li className="p-1 px-2 hover:underline-offset-8 hover:underline hover:cursor-pointer active:bg-teal-500 transition-all">
             {children}
           </li>
